955d0a5a92660356ef99a6adcff6f2487f987329
Robert Morelli <32861015+robertmorelli@users.noreply.github.com>
2024-02-09 21:47:38 -0700

    file headers

--- Spreadsheet/Spreadsheet.cs
+++ Spreadsheet/Spreadsheet.cs
@@ -1,4 +1,21 @@
-﻿using SpreadsheetUtilities;
+﻿/// <summary>
+/// Author:    Robert Morelli
+/// Partner:   None
+/// Date:      2-9-24
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and [Your Name(s)] - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Robert Morelli, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from code that is not my own. All 
+/// references to code that is not my own used in the completion of the
+/// assignments are cited in my README file.
+///
+/// File Contents:
+/// My implementation of AbstractSpreadsheet
+/// </summary>
+
+using SpreadsheetUtilities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
--- SpreadsheetTests/SpreadsheetTest.cs
+++ SpreadsheetTests/SpreadsheetTest.cs
@@ -1,3 +1,21 @@
+/// <summary>
+/// Author:    Robert Morelli
+/// Partner:   None
+/// Date:      2-9-24
+/// Course:    CS 3500, University of Utah, School of Computing
+/// Copyright: CS 3500 and [Your Name(s)] - This work may not 
+///            be copied for use in Academic Coursework.
+///
+/// I, Robert Morelli, certify that I wrote this code from scratch and
+/// did not copy it in part or whole from code that is not my own. All 
+/// references to code that is not my own used in the completion of the
+/// assignments are cited in my README file.
+///
+/// File Contents:
+/// My tests for my implementation of AbstractSpreadsheet
+/// </summary>
+
+
 namespace SpreadsheetTests
 {
     [TestClass]
