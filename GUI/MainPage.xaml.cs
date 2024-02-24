df3e9fb082f54898dbe3f1f9b88b1a5050c83a7e
Robert Morelli <32861015+robertmorelli@users.noreply.github.com>
2024-02-24 15:18:46 -0700

    basic spreadsheet functionality. add cells. cells update from deps. still no guidlines and labels

--- GUI/MainPage.xaml.cs
+++ GUI/MainPage.xaml.cs
@@ -1,42 +1,115 @@
-﻿namespace GUI
+﻿using Microsoft.Maui.Controls.Internals;
+using SS;
+
+namespace GUI
 {
     public partial class MainPage : ContentPage
     {
         static readonly int rows = 99;
         static readonly int columns = 26;
         static readonly int widths = 400;
-        static readonly int height = 30;
+        static readonly int heights = 30;
+        internal Spreadsheet spreadsheet;
+        internal Dictionary<string, Label> labels = [];
+        internal int entryI = 0;
+        internal int entryJ = 0;
         public MainPage()
         {
+            spreadsheet = new();
             InitializeComponent();
             VerticalStackLayout gridholder = FindByName("Grid") as VerticalStackLayout;
             Grid grid = new()
             {
                 RowDefinitions = new RowDefinitionCollection(
                     Enumerable.Range(0, rows)
-                    .Select((_) => new RowDefinition(height: height)).ToArray()), //.Where<RowDefinition>((_) => new RowDefinition()).ToArray(),
+                    .Select((_) => new RowDefinition(height: heights)).ToArray()), //.Where<RowDefinition>((_) => new RowDefinition()).ToArray(),
                 ColumnDefinitions = new ColumnDefinitionCollection(
                     Enumerable.Range(0, columns)
                     .Select((_) => new ColumnDefinition(width: widths)).ToArray()),
                 WidthRequest = columns * widths,
-                HeightRequest = rows * height,
-                BackgroundColor = Colors.Azure
+                HeightRequest = rows * heights,
+                BackgroundColor = Colors.DarkCyan
             };
-            for (int i = 0; i < rows; i++)
-                for (int j = 0; j < columns; j++)
-                    grid.Add(new Entry
-                    {
-                        BackgroundColor = Colors.Beige,
-                        HeightRequest = height,
-                        WidthRequest = widths,
-                    }, j, i);
+
+            TapGestureRecognizer taps = new();
+            taps.Tapped += (s, e) => OnGridTapped(s, e, grid);
+            grid.GestureRecognizers.Add(taps);
+
+
             gridholder?.Add(grid);
         }
 
-        private void SettupCellAdder(Entry entry, int j, int i)
+        private void OnGridTapped(object? _, TappedEventArgs e, Grid grid)
+        {
+            var pos = (Point)e.GetPosition(grid);
+            entryI = (int)(pos.X / widths);
+            entryJ = (int)(pos.Y / heights);
+            string cellName = getCellName(entryI, entryJ);
+
+
+            var entry = new Entry { BackgroundColor = Colors.Blue };
+
+
+            entry.Unfocused += (sender, e) =>
+            {
+                grid.Remove(entry);
+            };
+            entry.Completed += (sender, e) =>
+            {
+                try
+                {
+                    var toDo = spreadsheet.SetContentsOfCell(cellName, entry.Text);
+                    foreach (var item in toDo)
+                    {
+                        if (labels.TryGetValue(item, out var label))
+                        {
+                            grid.Remove(label);
+                            labels.Remove(item);
+                        }
+                        var value = spreadsheet.GetCellValue(item);
+                        var lab = new Label
+                        {
+                            BackgroundColor = Colors.Azure,
+                            Text = (value is FormatException exception) ? exception.Message : value.ToString()
+                        };
+                        labels.Add(item, lab);
+                        grid.Add(lab, RowFromCellName(item), ColFromCellName(item));
+                    }
+                    grid.Remove(entry);
+                }
+                catch {
+                    //manage errors for circular ...
+                }
+            };
+
+            grid.Add(entry, entryI, entryJ);
+            entry.Focus();
+        }
+
+        private void ChangeCell(int i, int j)
         {
 
         }
+
+        private void FileMenuNew(object sender, EventArgs e)
+        {
+
+            //if (spreadsheet.Changed)
+            //else spreadsheet.Save() { }
+        }
+
+        private void FileMenuOpenAsync(object sender, EventArgs e)
+        {
+
+        }
+
+        private string ColName(int i) => i.ToString();
+        private string RowName(int i) => "" + (char)('A' + i);
+        private string getCellName(int r, int c) => RowName(r) + ColName(c);
+
+        private int RowFromCellName(string name) => name[0] - 'A';
+        private int ColFromCellName(string name) => int.Parse(name[1..]);
+
     }
 }
 
@@ -53,11 +126,6 @@
 /*
  * 
  * 
- * <ContentPage.MenuBarItems>
-        <MenuBarItem Text="File">
-            <MenuFlyoutItem Text="New" Clicked="FileMenuNew" />
-            <MenuFlyoutItem Text="Open" Clicked="FileMenuOpenAsync" />
-        </MenuBarItem>
-    </ContentPage.MenuBarItems>
+ * 
 
 */
